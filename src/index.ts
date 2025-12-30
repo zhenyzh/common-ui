8fe70678cf5298a48fccf2b8c7ce1fd64eb07be3
Evgeny Zherdev <zheny.zherdev@bk.ru>
2025-12-30 10:46:17 +0700

    add icons

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+// components
 export * from "./components/skeleton";
 export * from "./components/button";
 export * from "./components/card";
 export * from "./components/typography";
 export * from "./components/container";
+
+//icon
+export * from './assets/icons'
