2597b6f9676a280a93674d3e789bc58dbb6510fb
Evgeny Zherdev <zheny.zherdev@bk.ru>
2025-12-30 11:30:53 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     build: {
         lib: {
             entry: {
+                index: path.resolve(__dirname, "src/index.ts"),
                 components: path.resolve(__dirname, "src/components/index.ts"),
                 icons: path.resolve(__dirname, 'src/assets/icons/index.ts'),
             },
