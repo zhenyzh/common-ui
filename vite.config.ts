0b82211ef5916c40e91ce61c78794483daefb453
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-01-06 13:33:49 +0700

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
 import path from 'path'
 
 export default defineConfig({
     plugins: [
         react(),
-        dts({
-            insertTypesEntry: true,
-            exclude: ["src/env.ts", "src/**/*.stories.ts"],
-        }),
-        cssInjectedByJsPlugin(),
+        dts({insertTypesEntry: true}),
     ],
 
     build: {
         lib: {
             entry: {
-                index: path.resolve(__dirname, 'src/index.ts'),
                 components: path.resolve(__dirname, 'src/components/index.ts'),
                 icons: path.resolve(__dirname, 'src/icons/index.ts'),
             },
