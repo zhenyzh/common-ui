e89c8ea10115f23a595178d0bce3f7de3c27d6d7
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-01-06 18:10:25 +0700

    add round

---
+++ src/icons/UserRound.tsx
@@ -0,0 +1,12 @@
+import type {SVGProps} from 'react'
+
+export const UserRound = (props: SVGProps<SVGSVGElement>) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}
+             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+        >
+            <circle cx="12" cy="8" r="5"/>
+            <path d="M20 21a8 8 0 0 0-16 0"/>
+        </svg>
+    )
+};
