79f635e648317c1b97fdf1213cb48dbfa45d3220
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-01-06 19:32:25 +0700

    add icon cart

--- src/icons/UserRoundIcon.tsx
+++ src/icons/UserRoundIcon.tsx
@@ -2,7 +2,7 @@ import type {SVGProps} from 'react'
 
 export const UserRoundIcon = (props: SVGProps<SVGSVGElement>) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={props.fontSize || 24} height={props.fontSize || 24}
+        <svg xmlns="http://www.w3.org/2000/svg" width={props.width || 24} height={props.height || 24}
              viewBox="0 0 24 24" fill="none" {...props}
              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
         >
