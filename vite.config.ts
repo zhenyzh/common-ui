e649b4052e495a4e497b679de0b3094a5bc9a6a2
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-01-06 13:13:00 +0700

    fix: package

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
             name: "common-ui",
-            formats: ['es', 'cjs'],
+            formats: ['es', 'cjs', 'umd'],
             fileName: format => `index.${format}.js`,
         },
         rollupOptions: {
