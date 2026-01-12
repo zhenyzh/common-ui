55f8e2e129bbb35702c835a97a81e5e12e36ce73
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-01-12 11:55:46 +0700

    fix UserRoundIcon

--- src/icons/UserRoundIcon.tsx
+++ src/icons/UserRoundIcon.tsx
@@ -1,13 +1,21 @@
-import type {SVGProps} from 'react'
+import type { SVGProps } from "react";
 
 export const UserRoundIcon = (props: SVGProps<SVGSVGElement>) => {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={props.width || 24} height={props.height || 24}
-             viewBox="0 0 24 24" fill="none" {...props}
-             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-        >
-            <circle cx="12" cy="8" r="5"/>
-            <path d="M20 21a8 8 0 0 0-16 0"/>
-        </svg>
-    )
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.width || 24}
+      height={props.height || 24}
+      viewBox="0 0 24 24"
+      fill="none"
+      {...props}
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="8" r="5" />
+      <path d="M20 21a8 8 0 0 0-16 0" />
+    </svg>
+  );
 };
