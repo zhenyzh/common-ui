131174d9f6cbdbe78692aba58a55a42855d65e48
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-01-12 13:48:38 +0700

    add outline in button

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,36 +1,39 @@
-import type { Meta, StoryObj } from '@storybook/react-vite'
+import type { Meta, StoryObj } from "@storybook/react-vite";
 
-import { Button } from './button'
+import { Button } from "./button";
 
 const meta = {
-  title: 'Components/Button',
+  title: "Components/Button",
   component: Button,
   parameters: {
-    layout: 'centered',
+    layout: "centered",
   },
   args: {},
-} satisfies Meta<typeof Button>
+} satisfies Meta<typeof Button>;
 
-export default meta
-type Story = StoryObj<typeof meta>
+export default meta;
+type Story = StoryObj<typeof meta>;
 
 export const AllButtons: Story = {
   render: () => (
     <div
       style={{
-        display: 'flex',
-        gap: '24px',
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: '250px',
-      }}>
+        display: "flex",
+        gap: "24px",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "250px",
+      }}
+    >
       <Button variant="primary">Primary</Button>
       <Button variant="secondary">Secondary</Button>
+      <Button variant="outline">Outline</Button>
       <Button fullWidth>Full Width</Button>
       <Button disabled>Disabled</Button>
-      <Button variant="primary" as="p" href="https://it-incubator.io/" target="_blank">
+
+      <Button variant="primary" as="p" href="https://" target="_blank">
         Link
       </Button>
     </div>
   ),
-}
+};
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,29 +1,34 @@
-import { clsx } from 'clsx'
-import type { ComponentProps, ElementType } from 'react'
+import { clsx } from "clsx";
+import type { ComponentProps, ElementType } from "react";
 
-import s from './button.module.css'
+import s from "./button.module.css";
 
-export type ButtonVariant = 'primary' | 'secondary'
+export type ButtonVariant = "primary" | "secondary" | "outline";
 
-export type ButtonProps<T extends ElementType = 'button'> = {
-  as?: T
-  fullWidth?: boolean
-  variant?: ButtonVariant
-} & ComponentProps<T>
+export type ButtonProps<T extends ElementType = "button"> = {
+  as?: T;
+  fullWidth?: boolean;
+  variant?: ButtonVariant;
+} & ComponentProps<T>;
 
-export const Button = <T extends ElementType = 'button'>({
-  as: Component = 'button',
+export const Button = <T extends ElementType = "button">({
+  as: Component = "button",
   children,
   className,
   fullWidth = false,
-  variant = 'primary',
+  variant = "primary",
   ...props
 }: ButtonProps<T>) => {
-  const classNames = clsx(s.button, s[variant], fullWidth && s.fullWidth, className)
+  const classNames = clsx(
+    s.button,
+    s[variant],
+    fullWidth && s.fullWidth,
+    className,
+  );
 
   return (
     <Component className={classNames} {...props}>
       {children}
     </Component>
-  )
-}
+  );
+};
