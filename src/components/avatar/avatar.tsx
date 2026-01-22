daa932b122158d0b1b4a36481944e99069598ccc
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-01-22 15:57:00 +0700

    fix avatar

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -1,19 +1,29 @@
 import s from "./avatar.module.css";
 import clsx from "clsx";
 
+export type AvatarVariant = "user" | "logo";
+
 export type AvatarProps = {
   image: string | React.ReactNode;
-  width?: number;
-  height?: number;
+  size?: string;
+  variant?: AvatarVariant;
   className?: string;
 };
 
-export const Avatar = ({ image, width, height, className }: AvatarProps) => {
+export const Avatar = ({
+  image,
+  size,
+  variant = "user",
+  className,
+}: AvatarProps) => {
   return (
-    <div className={clsx(s.card)}>
-      <div className={clsx(s.image, className)}>
+    <div
+      className={clsx(s.card, className)}
+      style={{ width: size, height: size }}
+    >
+      <div className={clsx(s.image, s[variant])}>
         {typeof image === "string" ? (
-          <img src={image} width={width} height={height} alt="" aria-hidden />
+          <img src={image} alt="" aria-hidden />
         ) : (
           image
         )}
