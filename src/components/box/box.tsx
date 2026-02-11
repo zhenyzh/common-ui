0d778459b621f6f48e86ea181e1aed719580da29
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-02-11 11:43:23 +0700

    fix

--- src/components/box/box.tsx
+++ src/components/box/box.tsx
@@ -7,7 +7,7 @@ export type BoxProps<T extends ElementType = "div"> = {
 export const Box = React.forwardRef(
   <T extends ElementType = "div">(
     { as = "div", ...props }: BoxProps<T>,
-    ref: React.Ref<T>,
+    ref: React.Ref<any>,
   ) => {
     const Component = as;
     return <Component ref={ref} {...props} />;
